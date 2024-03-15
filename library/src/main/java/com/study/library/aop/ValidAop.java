75998539519484a1c5556db908159c38f0beb134
GyunNote <rlaehrbs1968@naver.com>
2024-03-15 17:45:34 +0900

    2024-03-15

--- library/src/main/java/com/study/library/aop/ValidAop.java
+++ library/src/main/java/com/study/library/aop/ValidAop.java
@@ -1,15 +1,19 @@
 package com.study.library.aop;
 
 import com.study.library.aop.annotation.ValidAspect;
+import com.study.library.dto.SignupReqDto;
 import com.study.library.exception.ValidException;
+import com.study.library.repository.UserMapper;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 
 import javax.validation.ValidationException;
 import java.util.HashMap;
@@ -19,12 +23,15 @@ import java.util.Map;
 @Aspect
 @Component
 public class ValidAop {
+    @Autowired
+    private UserMapper userMapper;
 
     @Pointcut("@annotation(com.study.library.aop.annotation.ValidAspect)")
     private void pointCut(){}
 
     @Around("pointCut()")
     public Object around(ProceedingJoinPoint proceedingJoinPoint) throws Throwable{
+        String methodName = proceedingJoinPoint.getSignature().getName();
         Object[] args = proceedingJoinPoint.getArgs();
 
         BeanPropertyBindingResult bindingResult = null;
@@ -33,6 +40,18 @@ public class ValidAop {
            if(arg.getClass() == BeanPropertyBindingResult.class){
                bindingResult = (BeanPropertyBindingResult) arg;
            }
+        }
+        if(methodName.equals("signup")){
+            SignupReqDto signupReqDto = null;
+            for (Object arg : args){
+                if(arg.getClass() == SignupReqDto.class){
+                    signupReqDto = (SignupReqDto)arg;
+                }
+            }
+            if(userMapper.findUserByUsername(signupReqDto.getUsername()) != null){
+                ObjectError objectError = new FieldError("username", "username", "이미 존재하는 사용자 이름입니다");
+                bindingResult.addError(objectError);
+            }
         }
 
         if(bindingResult.hasErrors()){
--- library/src/main/java/com/study/library/controller/advice/ExceptionControllerAdvice.java
+++ library/src/main/java/com/study/library/controller/advice/ExceptionControllerAdvice.java
@@ -3,6 +3,8 @@ package com.study.library.controller.advice;
 import com.study.library.exception.SaveException;
 import com.study.library.exception.ValidException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,6 +13,7 @@ import javax.validation.ValidationException;
 
 @RestControllerAdvice
 public class ExceptionControllerAdvice {
+
     @ExceptionHandler(SaveException.class)
     public ResponseEntity<?> saveException(SaveException e){
         return ResponseEntity.internalServerError().body(e.getMessage());
@@ -19,5 +22,13 @@ public class ExceptionControllerAdvice {
     @ExceptionHandler(ValidException.class)
     public ResponseEntity<?> validException(ValidException e){
         return ResponseEntity.badRequest().body(e.getErrorMap());
+    }
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity<?> usernameNotFoundException(UsernameNotFoundException e){
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<?> badCredentialsException(BadCredentialsException e){
+        return ResponseEntity.badRequest().body(e.getMessage());
     }
 }
